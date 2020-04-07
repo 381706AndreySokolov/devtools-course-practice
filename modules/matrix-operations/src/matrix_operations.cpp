11658c1f73e8360ef8fa45c225c05779a0e69b02
Sokolov Andrey <Sokolov11.nn@gmail.com>
2020-04-07 14:16:23 +0300

    10

--- modules/matrix-operations/src/matrix_operations.cpp
+++ modules/matrix-operations/src/matrix_operations.cpp
@@ -122,72 +122,72 @@ bool Matrix::operator!= (const Matrix& _matrix) const {
     return !(*this == _matrix);
 }
 
-double Matrix::determinant() {
-    double sum12{0.0};
-    double sum22{0.0};
-    for (int idx{0}; idx < rows; idx++) {
-        double sum11 = 1.0;
-        double sum21 = 1.0;
-        int l = 2 * rows - 1 - idx;
-        for (int jdx{0}; jdx < rows; jdx++) {
-            sum21 *= data[jdx][l-- % rows];
-            sum11 *= data[jdx][(jdx + idx) % (rows)];
-        }
-        sum22 += sum21;
-        sum12 += sum11;
-    }
-    return sum12 - sum22;
-}
-
-Matrix Matrix::transpose() {
-    Matrix result(rows, cols, data);
-
-    for (int idx{0}; idx < getRows()-1; ++idx) {
-        for (int jdx{idx + 1}; jdx < getCols(); ++jdx) {
-            std::swap(result.data[jdx][idx], result.data[idx][jdx]);
-        }
-    }
-    return result;
-}
-
-Matrix Matrix::takeInverseMatrix() {
-    if (this->determinant() == 0) {
-        throw "Determinant are equal zero";
-    }
-
-    int size{rows};
-
-    Matrix A(size, size, data);
-    Matrix A0(size, size, data);
-    Matrix E2(size, size);
-    for (int idx{0}; idx < size; idx++) {
-        E2.data[idx][idx] = 2;
-    }
-
-    double N1{0.0};
-    double Ninf{0.0};
-    for (int row{0}; row < size; ++row) {
-        double colsum{0.0};
-        double rowsum{0.0};
-        for (int col{0}; col < size; ++col) {
-            rowsum += fabs(A0.data[row][col]);
-            colsum += fabs(A0.data[col][row]);
-        }
-        N1 = std::max(colsum, N1);
-        Ninf = std::max(rowsum, Ninf);
-    }
-
-    A0 = A0.transpose();
-    A0 = A0 * (1 / (N1 * Ninf));
-
-    Matrix inv{A0};
-    while (fabs((A * inv).determinant() - 1) >= 0.001) {
-        Matrix prev{inv};
-        inv = A * prev;
-        inv = inv * -1;
-        inv = inv + E2;
-        inv = prev * inv;
-    }
-
-    return inv;
-}
+//double Matrix::determinant() {
+//    double sum12{0.0};
+//    double sum22{0.0};
+//    for (int idx{0}; idx < rows; idx++) {
+//        double sum11 = 1.0;
+//        double sum21 = 1.0;
+//        int l = 2 * rows - 1 - idx;
+//        for (int jdx{0}; jdx < rows; jdx++) {
+//            sum21 *= data[jdx][l-- % rows];
+//            sum11 *= data[jdx][(jdx + idx) % (rows)];
+//        }
+//        sum22 += sum21;
+//        sum12 += sum11;
+//    }
+//    return sum12 - sum22;
+//}
+//
+//Matrix Matrix::transpose() {
+//    Matrix result(rows, cols, data);
+//
+//    for (int idx{0}; idx < getRows()-1; ++idx) {
+//        for (int jdx{idx + 1}; jdx < getCols(); ++jdx) {
+//            std::swap(result.data[jdx][idx], result.data[idx][jdx]);
+//        }
+//    }
+//    return result;
+//}
+//
+//Matrix Matrix::takeInverseMatrix() {
+//    if (this->determinant() == 0) {
+//        throw "Determinant are equal zero";
+//    }
+//
+//    int size{rows};
+//
+//    Matrix A(size, size, data);
+//    Matrix A0(size, size, data);
+//    Matrix E2(size, size);
+//    for (int idx{0}; idx < size; idx++) {
+//        E2.data[idx][idx] = 2;
+//    }
+//
+//    double N1{0.0};
+//    double Ninf{0.0};
+//    for (int row{0}; row < size; ++row) {
+//        double colsum{0.0};
+//        double rowsum{0.0};
+//        for (int col{0}; col < size; ++col) {
+//            rowsum += fabs(A0.data[row][col]);
+//            colsum += fabs(A0.data[col][row]);
+//        }
+//        N1 = std::max(colsum, N1);
+//        Ninf = std::max(rowsum, Ninf);
+//    }
+//
+//    A0 = A0.transpose();
+//    A0 = A0 * (1 / (N1 * Ninf));
+//
+//    Matrix inv{A0};
+//    while (fabs((A * inv).determinant() - 1) >= 0.001) {
+//        Matrix prev{inv};
+//        inv = A * prev;
+//        inv = inv * -1;
+//        inv = inv + E2;
+//        inv = prev * inv;
+//    }
+//
+//    return inv;
+//}
